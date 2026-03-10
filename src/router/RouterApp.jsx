44013ba81f11a19ccf5392d19ed3e1a6a23a389f
Agusmay <maydanaleonardoagustin@gmail.com>
2026-03-10 11:10:23 -0300

    feat: agregar pagina de /acerca

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -4,6 +4,7 @@ import { NotFound } from "../views/NotFound"
 import { Login } from "../views/Login"
 import { RouteProtected } from "../components/RouteProtected"
 import Register  from "../views/Register" // nueva ruta 
+import { Acerca } from "../views/Acerca"
 
 const RouterApp = () => {
   return (
@@ -12,7 +13,8 @@ const RouterApp = () => {
         <Route path="/" element={<RouteProtected><Home /></RouteProtected>} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/registro" element={<Register />} /> 
+        <Route path="/registro" element={<Register />} />
+        <Route path="/acerca" element={<Acerca />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/views/Acerca.jsx
@@ -0,0 +1,18 @@
+function Acerca() {
+  return (
+
+    <div>
+      <h1>Acerca del proyecto</h1>
+
+      <p> El proyecto fue creado como trabajo final del curso de Desarrollo Web Frontend. Donde se busca imitar whatsapp web, con el uso de tecnologias React, Javascript, HTML, CSS, Vite,
+        se usa context para presistencia de datos, localStorage para mantener sesion al recargar, el proyecto no tomo mucho tiempo hacer porque no hay una base de datos a la que conectar, es toda la parte externa de como se veria para el usuario y la experiencia que tenga. </p>
+        <h3>Decisiones sobre el proyecto</h3>
+        <p>decisiones no tan complejas, preferi ir por un trabajo sencillo pero bien realizado, problemas con los horarios de trabajo y hallar un ritmo, opte por utilidad en cuanto complejo se riefe y agradable estetica visual </p>
+
+      <p>El objetivo es aprender manejo de rutas, formularios y persistencia de usuario.</p>
+
+    </div>
+  );
+}
+
+export default { Acerca }
